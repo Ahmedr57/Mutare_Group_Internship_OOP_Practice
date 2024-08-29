a28560926a653e30d775cfadcc187a22b805a6ef
Muhammad Ahmed Raza <f21bdats1m02048@iub.edu.pk>
2024-08-29 21:33:08 +0500

    Question 3 solved

---
+++ Question_3.py
@@ -0,0 +1,36 @@
+# Design an abstract class Shape with an abstract method area(). Then create two derived classes, Rectangle and Circle, 
+# that implement the area() method for calculating the area of a rectangle and a circle, respectively. 
+# Demonstrate the use of these classes by creating objects and calculating areas.
+import math 
+class Shape:
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        Area = self.length * self.width
+        print(f"Area of Rectangle is: {Area}")
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        Area = math.pi * self.radius**2
+        print(f"Area of Circle is: {Area:.2f}")
+
+cir1 = Circle(50)
+cir1.area()
+
+rec1 = Rectangle(20,43)
+rec1.area()
+
+cir2 = Circle(6.1)
+cir2.area()
+
+rec2 = Rectangle(5,3)
+rec2.area()
